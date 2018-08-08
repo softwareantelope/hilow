107b074b534c75d4a94f6aa1aebd7b64d9387dde
nalex <work@sfsw.net>
2018-08-08 21:43:37 +0000

    keep scores

--- app/Http/Controllers/CardController.php
+++ app/Http/Controllers/CardController.php
@@ -8,6 +8,7 @@ use Illuminate\Support\Facades\Session;
 
 class CardController extends Controller
 {
+
     // shuffle the deck
     private function shuffle($cards) {
         $cards = json_decode($cards);
@@ -22,10 +23,15 @@ class CardController extends Controller
         $endpoint = 'https://cards.davidneal.io/api/cards';
         $cardsJson = file_get_contents($endpoint);
         $cards = $this->shuffle($cardsJson);
+        // store the deck
         Session::put('deck', $cards);
+        // zero the scores
+        Session::put('hits', 0);
+        Session::put('misses', 0);
         return view('cards', ['cards' => $cards, 'result' => null]);
     }
 
+    // A and court cards need to have values
     private function valueof($card) {
         preg_match('/([0-9A-Z]+) ([\w]+)/', $card, $matched);
         if (isset($matched)  && count($matched)>1) {
@@ -72,22 +78,34 @@ class CardController extends Controller
         $original = $request->input('card');
 
         $deck = Session::get('deck');
+        $hits = Session::get('hits');
+        $misses = Session::get('misses');
 
         $nextcard = $deck[1]->value . ' '. $deck[1]->suit;
         array_shift($deck);
 
+        // remember the deck
         Session::put('deck', $deck);
 
+        // compare the coards
         $result = $this->compare($original, $nextcard);
         $msg = $choice . ' was ';
         if ($result === $high || $result === $low) {
             $msg .= "CORRECT";
+            $hits++;
         } else {
-            $msg .= "NOT CORRECT";     
+            $msg .= "NOT CORRECT";
+            $misses++;
         }
+
+        // keep score
+        Session::put('hits', $hits);
+        Session::put('misses', $misses);
+
+        // precompose messages for View (TODO: use a view composer)
         $msg .= ', the (previous) card was '.$original;
         $info = 'There are '.count($deck). ' cards left in the deck.';
 
-        return view('cards', ['cards' => $deck, 'result' => $msg, 'info' => $info]);
+        return view('cards', ['cards' => $deck, 'result' => $msg, 'info' => $info, 'hits' => $hits, 'misses' => $misses ]);
     }
 }
--- resources/views/cards.blade.php
+++ resources/views/cards.blade.php
@@ -6,6 +6,7 @@
     <div id="firstCard" class="larger">
         {{ $cards[0]->value }} {{ $cards[0]->suit }} 
     </div>
+    @if(count($cards) > 1)
     <div class="large">
         Guess if the next card is higher or lower than this card?
         <form action="nextcard" method="get" class="question">
@@ -19,18 +20,22 @@
             <button>LOWER</button>
         </form>
     </div>
+    @else
+    <div class="large">There are no cards left!</div>
+    @endif
     @if($result)
     <div>
-        
-        <div class="large"><p><em>Result</em></p> {{ $result }}</div>
+        <div class="large">
+            <p><em>Result</em></p> 
+            {{ $result }}
+            <div>Hits {{ $hits }}</div>
+            <div>Misses {{ $misses }} </div>
+        </div>
         <div>{{$info}}</div>
     </div>
     <h5>
         <a href="/cards">Reshuffle</a>
     </h5>
     @endif
-</div>
-<div id="app">
-    <cards-component></cards-component>
 </div>
 @endsection
